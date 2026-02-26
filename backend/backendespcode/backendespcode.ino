0760f6f712f7fdcb25cce5243fadba386393ce38
TAPANANSHUTRIPATHY <tapananshutripathy06@gmail.com>
2026-02-26 21:05:05 +0530

    Create backendespcode.ino

---
+++ backend/backendespcode/backendespcode.ino
@@ -0,0 +1,79 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "DHT.h"
+
+#define DHTPIN 15
+#define DHTTYPE DHT11
+#define MQ135_PIN 34
+
+const char* ssid = "Saion";
+const char* password = "Saion123";
+
+// Replace with your laptop IP
+const char* serverUrl = "http://10.126.88.33:5000/api/update";
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected to WiFi");
+  Serial.print("ESP IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  int mqRaw = analogRead(MQ135_PIN);
+
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("DHT Read Failed!");
+    delay(3000);
+    return;
+  }
+
+  Serial.println("\nSending Data...");
+  Serial.print("Temp: ");
+  Serial.print(temperature);
+  Serial.print(" | Hum: ");
+  Serial.print(humidity);
+  Serial.print(" | Air: ");
+  Serial.println(mqRaw);
+
+  if (WiFi.status() == WL_CONNECTED) {
+
+    HTTPClient http;
+    http.begin(serverUrl);
+    http.addHeader("Content-Type", "application/json");
+
+    String jsonData = "{";
+    jsonData += "\"temperature\":" + String(temperature) + ",";
+    jsonData += "\"humidity\":" + String(humidity) + ",";
+    jsonData += "\"air_quality\":" + String(mqRaw);
+    jsonData += "}";
+
+    int httpResponseCode = http.POST(jsonData);
+
+    Serial.print("HTTP Response: ");
+    Serial.println(httpResponseCode);
+
+    if (httpResponseCode > 0) {
+      Serial.println(http.getString());
+    }
+
+    http.end();
+  }
+
+  delay(5000);
+}
